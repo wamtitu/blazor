dc3cd838226b8ea4c9aadb8bc72ce4994dbfdd7b
wamtitu <wamtitujose@gmail.com>
2023-09-15 11:47:50 +0300

    assessment week7

---
+++ DriveThrough/Models/ToastMessage.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DriveThrough.Models
+{
+    public class ToastMessage
+    {
+        public string Title {get; set;}
+        public string Type {get; set;}
+        public string HelpText {get; set;}
+        public string Message {get; set;}
+    }
+}
---
+++ Library/Models/Book.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.Models
+{
+    public class Book
+    {
+        public Guid BookId {get; set;}
+        public string Title{get; set;}
+        public string description{get; set;}
+        public string Authors {get; set;}
+    }
+}
