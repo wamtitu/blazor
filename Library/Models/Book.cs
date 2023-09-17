c1eb2f562914ab7371f9d93d37631f3337610220
wamtitu <wamtitujose@gmail.com>
2023-09-17 12:15:12 +0300

    added table for better ux

--- Library/Models/Book.cs
+++ Library/Models/Book.cs
@@ -7,9 +7,15 @@ namespace Library.Models
 {
     public class Book
     {
+        
         public Guid BookId {get; set;}
         public string Title{get; set;}
         public string description{get; set;}
-        public List<string> Authors {get; set;}
+        public List<string>? Authors {get; set;}
+
+         public Book()
+        {
+        Authors = new List<string>();
+            }
     }
 }
